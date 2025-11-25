7d8dc2b584b22ea1f9d264c197f87b49f2954492
Sérgio Filho <svf@cin.ufpe.br>
2025-11-25 19:56:42 -0300

    feat(algorithm): add noise free execution

--- src/main.py
+++ src/main.py
@@ -13,12 +13,14 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 load_dotenv()
 
 
+def create_results(string_c, string_m):
+    return [1 if string_c[i : i + len(string_m)] == string_m else 0 for i in range(len(string_c) - len(string_m) + 1)]
+
+
 def create_string_match(string_c, string_m):
     if len(string_m) > len(string_c):
         raise ValueError("String match is bigger than string to compare")
-    results = [
-        1 if string_c[i : i + len(string_m)] == string_m else 0 for i in range(len(string_c) - len(string_m) + 1)
-    ]
+    results = create_results(string_c, string_m)
 
     n_qubits_in = math.ceil(math.log2(len(results)))
     if n_qubits_in < 4:
@@ -72,11 +74,27 @@ def create_diffusor(qc, reg_in):
     qc.h(reg_in)
 
 
-string_comparison = "Eu como banana e maçã pela tarde, e outra banana de noite!"
-string_match = "banana"
+string_comparison = "sergio encontrou uma nova estrategia para melhorar o algoritmo"
+string_match = "melhorar o algoritmo"
+results = create_results(string_comparison, string_match)
 
 qc = create_string_match(string_comparison, string_match)
 service = QiskitRuntimeService(channel=os.getenv("IBM_QUANTUM_SERVICE"))
+
+backend = AerSimulator()
+pass_manager = generate_preset_pass_manager(
+    optimization_level=3,
+    backend=backend,
+    layout_method="sabre",
+    routing_method="sabre",
+)
+qc_best = pass_manager.run(qc)
+result = backend.run(qc, shots=2048).result()
+counts = result.get_counts()
+plot_histogram(counts)
+plt.title(f"Histogram for backend {backend.name}")
+plt.tight_layout()
+plt.show()
 
 for backend in service.backends():
     if backend.name == "ibm_marrakesh":
@@ -92,7 +110,7 @@ for backend in service.backends():
         routing_method="sabre",
     )
     qc_best = pass_manager.run(qc)
-    result = backend_noisy.run(qc_best, shots=1024).result()
+    result = backend_noisy.run(qc_best, shots=2048).result()
     counts = result.get_counts()
     plot_histogram(counts)
     plt.title(f"Histogram for backend {backend.name}")
